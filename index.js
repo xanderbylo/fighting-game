a3b010b2ab3717f2414fc6feae1107d31ea3147c
Xander Bylo <xanderbylo@gmail.com>
2022-04-02 23:53:57 +0100

    Edit index.js
    Add lastkey to improve left and right movement

--- index.js
+++ index.js
@@ -56,12 +56,31 @@ const enemy = new Sprite({
 
 console.log(player)
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+let lastKey
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
 }
 
 animate()
@@ -69,10 +88,12 @@ animate()
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 1
+            keys.d.pressed = true
+            lastKey = 'd'
             break
         case 'a':
-            player.velocity.x = -1
+            keys.a.pressed = true
+            lastKey = 'a'
             break
     }
     console.log(event.key);
@@ -81,10 +102,10 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 0
+            keys.d.pressed = false
             break
         case 'a':
-            player.velocity.x = 0
+            keys.a.pressed = false
             break
     }
     console.log(event.key);
