98d4bc9adb27a248c455246c5d049fce48e82b4d
Xander Bylo <xanderbylo@gmail.com>
2022-04-02 23:36:53 +0100

    Edit index.js
    Add left and right movement for player using a and d keys

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ class Sprite {
     update() {
         this.draw()
         
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -64,3 +65,27 @@ function animate() {
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            player.velocity.x = 1
+            break
+        case 'a':
+            player.velocity.x = -1
+            break
+    }
+    console.log(event.key);
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            player.velocity.x = 0
+            break
+        case 'a':
+            player.velocity.x = 0
+            break
+    }
+    console.log(event.key);
+})
