820528bc8160998b1a99c68f8f468a79cff94b7a
Xander Bylo <xanderbylo@gmail.com>
2022-04-17 23:25:45 +0100

    Edit index.js and classes.js
    Add jump animation to player

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ const player = new Fighter({
     },
     imageSrc: './img/player/idle.png',
     framesMax: 8,
-    scale: 2.5,
     offset: { 
         x: 215,
         y: 155
@@ -44,6 +43,10 @@ const player = new Fighter({
         run: {
             imageSrc: './img/player/run.png',
             framesMax: 8
+        },
+        jump: {
+            imageSrc: './img/player/jump.png',
+            framesMax: 2
         }
     }
 })
@@ -95,13 +98,19 @@ function animate() {
     enemy.velocity.x = 0
 
     // player movement
-    player.image = player.sprites.idle.image
+    
     if (keys.a.pressed && player.lastKey === 'a') {
         player.velocity.x = -5
-        player.image = player.sprites.run.image
+        player.switchSprite('run')
     } else if (keys.d.pressed && player.lastKey === 'd') {
         player.velocity.x = 5
-        player.image = player.sprites.run.image
+        player.switchSprite('run')
+    } else {
+        player.switchSprite('idle')
+    }
+
+    if (player.velocity.y < 0) {
+        player.switchSprite('jump')
     }
 
     // enemy movement
