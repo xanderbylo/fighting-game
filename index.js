01dca34132a60b3b1bcb82dd84671cec50260292
Xander Bylo <xanderbylo@gmail.com>
2022-05-03 22:13:28 +0100

    Edit index.js and classes.js
    Add death animation to player and enemy

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ const player = new Fighter({
         takeHit: {
             imageSrc: './img/player/hit2.png',
             framesMax: 4
+        },
+        death: {
+            imageSrc: './img/player/death.png',
+            framesMax: 6
         }
     },
     attackBox: {
@@ -115,6 +119,10 @@ const enemy = new Fighter({
         takeHit: {
             imageSrc: './img/enemy/hit2.png',
             framesMax: 4
+        },
+        death: {
+            imageSrc: './img/enemy/death.png',
+            framesMax: 6
         }
     },
     attackBox: {
@@ -240,36 +248,44 @@ function animate() {
 animate()
 
 window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'd':
-            keys.d.pressed = true
-            player.lastKey = 'd'
-            break
-        case 'a':
-            keys.a.pressed = true
-            player.lastKey = 'a'
-            break
-        case 'w':
-           player.velocity.y = -20
-            break
-        case 's':
-            player.attack()
-            break
+    if (!player.dead) {
+    
+        switch (event.key) {
+            case 'd':
+                keys.d.pressed = true
+                player.lastKey = 'd'
+                break
+            case 'a':
+                keys.a.pressed = true
+                player.lastKey = 'a'
+                break
+            case 'w':
+            player.velocity.y = -20
+                break
+            case 's':
+                player.attack()
+                break
+        }
+    }
 
-        case 'ArrowRight':
-            keys.ArrowRight.pressed = true
-            enemy.lastKey = 'ArrowRight'
-            break
-        case 'ArrowLeft':
-            keys.ArrowLeft.pressed = true
-            enemy.lastKey = 'ArrowLeft'
-            break
-        case 'ArrowUp':
-            enemy.velocity.y = -20
-            break
-        case 'ArrowDown':
-            enemy.attack()
-            break
+    if (!enemy.dead) {
+    
+        switch(event.key) {   
+            case 'ArrowRight':
+                keys.ArrowRight.pressed = true
+                enemy.lastKey = 'ArrowRight'
+                break
+            case 'ArrowLeft':
+                keys.ArrowLeft.pressed = true
+                enemy.lastKey = 'ArrowLeft'
+                break
+            case 'ArrowUp':
+                enemy.velocity.y = -20
+                break
+            case 'ArrowDown':
+                enemy.attack()
+                break
+        }
     }
 })
 
