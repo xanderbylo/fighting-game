5f4c0d5ac0f823625440984fba35b03f09e5a6bf
Xander Bylo <xanderbylo@gmail.com>
2022-05-06 23:10:38 +0100

    Edit index.html and index.js
    Animate player and enemy health bar decrease using gsap

--- index.js
+++ index.js
@@ -212,7 +212,10 @@ function animate() {
     ) {
         enemy.takeHit()
         player.isAttacking = false
-        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+
+        gsap.to('#enemyHealth', {
+            width: enemy.health + '%'
+        })
     }
 
     // if player misses
@@ -231,7 +234,10 @@ function animate() {
     ) {
         player.takeHit()
         enemy.isAttacking = false
-        document.querySelector('#playerHealth').style.width = player.health + '%'
+
+        gsap.to('#playerHealth', {
+            width: player.health + '%'
+        })
     }
 
     // if enemy misses
